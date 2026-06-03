4cae8896044fdb44deaa045cc6fee943487a209f
Drangello <drangello0505@gmail.com>
2026-06-03 11:38:51 +0200

    Change Api URL to production backend

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -8,9 +8,8 @@ const GUEST_LOGINS = {
         password: 'asdasd24'
     }
 }
-
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'http://api.coderrfw.de:8000/api/';
+const STATIC_BASE_URL = 'http://api.coderrfw.de:8000/';
 
 
 const LOGIN_URL = 'login/';
